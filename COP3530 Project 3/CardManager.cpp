0fe6ec62876f4d3ccb2c9bbba8aab8dd08fac620
jiyansh <jiyansh1414@gmail.com>
2021-08-02 16:08:54 -0400

    Updated card manager can now be told which cards to display and how many by using VIN.

--- COP3530 Project 3/CardManager.cpp
+++ COP3530 Project 3/CardManager.cpp
@@ -1,10 +1,13 @@
 #include "CardManager.h"
 #include "SearchCard.h"
 
-CardManager::CardManager(float x, float y, float width, float height, float card_height, const ResourceManager<ImageDownloader>& resources)
-	: Panel(x, y, width, height), resources(resources) {
+CardManager::CardManager(float x, float y, float width, float height, float card_height, 
+	const std::vector<std::string>& cards_to_show, const ResourceManager<ImageDownloader>& resources)
+	: Panel(x, y, width, height), cards_to_show(cards_to_show), resources(resources) {
 	prev_card_y = -card_height - 15;
 	this->card_height = card_height;
+	num_cards_to_show = NUM_CARDS_TO_SHOW;
+	index_shown = 0;
 }
 
 CardManager::~CardManager() {
@@ -12,8 +15,51 @@ CardManager::~CardManager() {
 }
 
 void CardManager::AddCard(CarData data) {
-	AddObject(new SearchCard(5, prev_card_y + card_height + 20, width - 30, card_height, data, resources), 0);
-	prev_card_y = prev_card_y + card_height + 20;
+	cards.emplace(data.car_VIN, new SearchCard(5, 0, width - 30, card_height, data, resources));
+}
+
+void CardManager::UpdateCardsToShow() {
+	objectsToDraw.clear();
+	focused_object = nullptr;
+	prev_card_y = -card_height - 15;
+
+	unsigned int cards_added = 0;
+
+	for (unsigned int i = 0; i < cards_to_show.size() && cards_added < num_cards_to_show; i++) {
+		if (cards.find(cards_to_show[i]) != cards.end()) {
+			SearchCard* card = cards[cards_to_show[i]];
+			card->SetY(prev_card_y + card_height + 20);
+			prev_card_y = prev_card_y + card_height + 20;
+
+			AddObject(card, 0);
+			cards_added++;
+		}
+		index_shown = i;
+	}
+}
+
+void CardManager::ShowMoreCards(unsigned int num) {
+	unsigned int cards_added = 0;
+
+	for (unsigned int i = index_shown + 1; i < cards_to_show.size() && cards_added < num; i++) {
+		if (cards.find(cards_to_show[i]) != cards.end()) {
+			SearchCard* card = cards[cards_to_show[i]];
+			card->SetY(prev_card_y + card_height + 20);
+			prev_card_y = prev_card_y + card_height + 20;
+
+			AddObject(card, 0);
+			cards_added++;
+		}
+		index_shown = i;
+	}
+}
+
+void CardManager::SetWidth(float num) {
+	Panel::SetWidth(num);
+
+	for (auto iter = cards.begin(); iter != cards.end(); iter++) {
+		iter->second->SetWidth(num - 30);
+	}
 }
 
 void CardManager::Clear() {
--- COP3530 Project 3/CardManager.h
+++ COP3530 Project 3/CardManager.h
@@ -1,19 +1,34 @@
 #pragma once
+#include <unordered_map>
+#include <vector>
 #include <Panel.h>
 #include "CarData.h"
+#include "SearchCard.h"
 #include "ResourceManager.h"
 #include "ImageDownloader.h"
 
 class CardManager : public Panel {
 public:
-	CardManager(float x, float y, float width, float height, float card_height, const ResourceManager<ImageDownloader>& resources);
+	const static unsigned int NUM_CARDS_TO_SHOW = 25;
+
+	CardManager(float x, float y, float width, float height, float card_height, 
+		const std::vector<std::string>& cards_to_show, const ResourceManager<ImageDownloader>& resources);
 	~CardManager();
 
 	void AddCard(CarData data);
+	void UpdateCardsToShow();
+	void ShowMoreCards(unsigned int num);
+
+	void SetWidth(float num) override;
+
 	void Clear();
 
 private:
+	std::unordered_map<std::string, SearchCard*> cards;
+	const std::vector<std::string>& cards_to_show;
 	const ResourceManager<ImageDownloader>& resources;
+
 	float card_height;
 	float prev_card_y;
+	unsigned int num_cards_to_show, index_shown;
 };
