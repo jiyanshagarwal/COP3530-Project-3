ee27c619b6313f9628f9ee3d3313e33d90e74bf2
jiyansh <jiyansh1414@gmail.com>
2021-08-02 04:20:09 -0400

    Added multithreading support to image downloader. Now, all image urls will be automatically downloaded in the background to prevent lag.

--- COP3530 Project 3/ImageDownloader.cpp
+++ COP3530 Project 3/ImageDownloader.cpp
@@ -1,6 +1,7 @@
 #include "ImageDownloader.h"
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <thread>
 #include <Windows.h>
 #include <WinInet.h>
 
@@ -10,6 +11,45 @@ struct ImageDownloader::HandleWrapper {
 };
 
 ImageDownloader::HandleWrapper::HandleWrapper(HINTERNET& handle) : handle(handle) {}
+
+ImageDownloader::ImageDownloader(std::string app_name, std::vector<std::vector<std::string>>& vehicles) : app_name(app_name), loaded_index(0) {
+	int index = 0;
+
+	for (auto vehicle : vehicles) {
+		std::string image_url = vehicle[DataReader::IMAGE_URL];
+
+		url_to_image_map.emplace(image_url, index++);
+		images.push_back(std::make_tuple(image_url, false, std::vector<char>()));
+	}
+
+	downloader_thread = std::thread(&ImageDownloader::DownloadAllImages, this);
+}
+
+ImageDownloader::~ImageDownloader() {
+	downloader_thread.join();
+}
+
+bool ImageDownloader::GetImage(std::string image_url, sf::Texture& texture) {
+	try {
+		int index = url_to_image_map.at(image_url);
+
+		//This index check is very important to prevent race conditions where the main thread reads data that the downloader_thread could be modifying.
+		if (index < loaded_index) {
+			bool image_loaded = std::get<1>(images[index]);
+
+			if (image_loaded) {
+				std::vector<char>& image_data = std::get<2>(images[index]);
+				texture.loadFromMemory(image_data.data(), image_data.size());
+				return true;
+			}
+		}
+	}
+	catch (const std::out_of_range& e) {
+		return false;
+	}
+
+	return false;
+}
 
 bool ImageDownloader::DownloadImage(std::string app_name, std::string url, sf::Texture& texture) {
 	/*
@@ -61,6 +101,14 @@ bool ImageDownloader::DownloadImage(std::string app_name, std::string url, sf::T
 	return false;
 }
 
+void ImageDownloader::DownloadAllImages() {
+	for (unsigned int i = 0; i < images.size(); i++) {
+		std::string image_url = std::get<0>(images[i]);
+		std::get<1>(images[i]) = DownloadImage(image_url, std::get<2>(images[i]));
+		loaded_index++;
+	}
+}
+
 std::string ImageDownloader::GetHeaderAttribute(const HandleWrapper& handle, int header_attribute) {
 	/*
 	* Because of the stupid way that HttpQueryInfo works.You have to first call it so that it can put the buffer size in dwSize.
@@ -102,5 +150,75 @@ bool ImageDownloader::GetImage(const HandleWrapper& handle, sf::Texture& texture
 	} while (dwBytesRead != 0);
 
 	texture.loadFromMemory(data.data(), data.size());
+	return true;
+}
+
+bool ImageDownloader::DownloadImage(std::string url, std::vector<char>& image_data) {
+	/*
+	* The following pages were helpful for this:
+	* https://docs.microsoft.com/en-us/windows/win32/wininet/http-sessions
+	*/
+
+	//See https://docs.microsoft.com/en-us/windows/win32/api/wininet/nf-wininet-internetopena
+	HINTERNET session_handle = InternetOpenA(
+		app_name.c_str(),						//Name of the calling application
+		INTERNET_OPEN_TYPE_PRECONFIG,			//Access type. INTERNET_OPEN_TYPE_PRECONFIG is the default option
+		NULL,									//Name of proxy server. Only used when access type is INTERNET_OPEN_TYPE_PROXY.
+		NULL,									//Proxy bypass: Names of hosts and IP addresses to not route through when using proxies. Only used when access type is INTERNET_OPEN_TYPE_PROXY.
+		0										//Any flags.
+	);
+
+	if (session_handle) {
+		//See https://docs.microsoft.com/en-us/windows/win32/api/wininet/nf-wininet-internetopenurla
+		HINTERNET internet_open_handle = InternetOpenUrlA(
+			session_handle,						//Handle to result from InternetOpen.
+			url.c_str(),						//URL to open.
+			NULL,								//Any additional headers to send.
+			0,									//Size of additional headers.
+			INTERNET_FLAG_RELOAD |				//Any Flags. Reload forces download of requested file from server, not cache.
+			INTERNET_FLAG_SECURE,				//Secure uses SSL so that HTTPS is used not HTTP.
+			0									//Application context.
+		);
+
+		if (internet_open_handle) {
+			std::string status_code = GetHeaderAttribute(HandleWrapper(internet_open_handle), HTTP_QUERY_STATUS_CODE);
+			std::string content_type = GetHeaderAttribute(HandleWrapper(internet_open_handle), HTTP_QUERY_CONTENT_TYPE);
+
+			if ((status_code == "200" || status_code == "304")
+				&& (content_type == "image/jpeg" || content_type == "image/jpg" || content_type == "image/png")) {
+
+				if (GetImage(HandleWrapper(internet_open_handle), image_data)) {
+					InternetCloseHandle(internet_open_handle);
+					InternetCloseHandle(session_handle);
+					return true;
+				}
+			}
+
+			InternetCloseHandle(internet_open_handle);
+		}
+
+		InternetCloseHandle(session_handle);
+	}
+
+	return false;
+}
+
+bool ImageDownloader::GetImage(const HandleWrapper& handle, std::vector<char>& image_data) {
+	image_data.clear();
+	const DWORD dwBytesToRead = 5000;
+	DWORD dwBytesRead = 0;
+
+	do {
+		const size_t old_size = image_data.size();
+		image_data.resize(image_data.size() + dwBytesToRead);
+
+		if (!InternetReadFile(handle.handle, &image_data[old_size], dwBytesToRead, &dwBytesRead)) {
+			image_data.clear();
+			return false;
+		}
+
+		image_data.resize(old_size + dwBytesRead);
+	} while (dwBytesRead != 0);
+
 	return true;
 }
--- COP3530 Project 3/ImageDownloader.h
+++ COP3530 Project 3/ImageDownloader.h
@@ -1,13 +1,37 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include <unordered_map>
 #include <string>
+#include <vector>
+#include <tuple>
+#include <atomic>
+#include <thread>
+#include "DataReader.h"
 
 class ImageDownloader {
 public:
+	ImageDownloader(std::string app_name, std::vector<std::vector<std::string>>& vehicles);
+	ImageDownloader(const ImageDownloader& ref) = delete;
+	ImageDownloader& operator=(const ImageDownloader& ref) = delete;
+	~ImageDownloader();
+
+	bool GetImage(std::string image_url, sf::Texture& texture);
 	static bool DownloadImage(std::string app_name, std::string url, sf::Texture& texture);
+
 private:
 	struct HandleWrapper;	//HandleWrapper is needed to pass the InternetOpenURL handle between functions without polluting the header namespace with Windows.h.
 
+	std::unordered_map<std::string, int> url_to_image_map;
+	std::vector<std::tuple<std::string, bool, std::vector<char>>> images;
+	std::atomic<int> loaded_index;
+	std::thread downloader_thread;
+
+	std::string app_name;
+
+	void DownloadAllImages();
 	static std::string GetHeaderAttribute(const HandleWrapper& handle, int header_attribute);
 	static bool GetImage(const HandleWrapper& handle, sf::Texture& texture);
+
+	bool DownloadImage(std::string url, std::vector<char>& image_data);
+	bool GetImage(const HandleWrapper& handle, std::vector<char>& image_data);
 };
