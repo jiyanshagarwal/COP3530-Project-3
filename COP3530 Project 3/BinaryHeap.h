1d37f2cfe5540bd982951d566e0ae57f05f772a9
jiyansh <jiyansh1414@gmail.com>
2021-08-02 21:04:01 -0400

    Added key-value functionality to binary heap.

--- COP3530 Project 3/BinaryHeap.h
+++ COP3530 Project 3/BinaryHeap.h
@@ -3,67 +3,67 @@
 #include <cmath>
 #include <stdexcept>
 
-template <typename T>
+template <typename K, typename T>
 class BinaryHeap {
 public:
 	BinaryHeap(bool is_min_heap = true);
-	BinaryHeap(std::vector<T> vec, bool is_min_heap = true);
+	BinaryHeap(std::vector<std::pair<K, T>>, bool is_min_heap = true);
 
-	void Insert(T item);
+	void Insert(K key, T item);
 	T& Front();
 	void Remove();
 
-	static void HeapSort(std::vector<T>& vec, bool is_min_heap = true);
+	static void HeapSort(std::vector<std::pair<K, T>>& vec, bool is_min_heap = true);
 	unsigned int Size() const;
 	bool isMinHeap() const;
 	void Clear();
 
 private:
-	std::vector<T> heap;
+	std::vector<std::pair<K, T>> heap;
 	unsigned int size;
 	bool is_min_heap;
 
-	bool Compare(T& item1, T& item2) const;
+	bool Compare(K& item1, K& item2) const;
 	void HeapifyDown(unsigned int index);
 
-	static bool Compare(T& item1, T& item2, bool is_min_heap);
-	static void HeapifyDown(std::vector<T>& vec, unsigned int size, unsigned int index, bool is_min_heap);
+	static bool Compare(K& item1, K& item2, bool is_min_heap);
+	static void HeapifyDown(std::vector<std::pair<K, T>>& vec, unsigned int size, unsigned int index, bool is_min_heap);
 };
 
-template <typename T>
-BinaryHeap<T>::BinaryHeap(bool is_min_heap) : is_min_heap(is_min_heap), size(0) {}
+template <typename K, typename T>
+BinaryHeap<K, T>::BinaryHeap(bool is_min_heap) : is_min_heap(is_min_heap), size(0) {}
 
-template <typename T>
-BinaryHeap<T>::BinaryHeap(std::vector<T> vec, bool is_min_heap) : heap(vec), is_min_heap(is_min_heap), size(vec.size()) {
+template <typename K, typename T>
+BinaryHeap<K, T>::BinaryHeap(std::vector<std::pair<K, T>> vec, bool is_min_heap) : heap(vec), is_min_heap(is_min_heap), size(vec.size()) {
 	for (int i = size / 2; i >= 0; i--) {
 		HeapifyDown(i);
 	}
 }
 
-template <typename T>
-void BinaryHeap<T>::Insert(T item) {
-	if (heap.size() > size) heap[size] = item;
-	else heap.push_back(item);
+template <typename K, typename T>
+void BinaryHeap<K, T>::Insert(K key, T item) {
+	if (heap.size() > size) heap[size] = std::make_pair(key, item);
+	else heap.push_back(std::make_pair(key, item));
 	size++;
 
 	int child = size - 1;
 	int parent = std::floor((child - 1) / 2);
 
-	while (parent >= 0 && Compare(heap[child], heap[parent])) {
+	while (parent >= 0 && Compare(heap[child].first, heap[parent].first)) {
 		std::swap(heap[parent], heap[child]);
 		child = parent;
 		parent = std::floor((child - 1) / 2);
 	}
 }
 
-template <typename T>
-T& BinaryHeap<T>::Front() {
+template <typename K, typename T>
+T& BinaryHeap<K, T>::Front() {
 	if (size == 0) throw std::out_of_range("Cannot call front on empty binary heap.");
-	return heap[0];
+	return heap[0].second;
 }
 
-template <typename T>
-void BinaryHeap<T>::Remove() {
+template <typename K, typename T>
+void BinaryHeap<K, T>::Remove() {
 	if (size == 0) return;
 	if (size == 1) {
 		heap.clear();
@@ -77,8 +77,8 @@ void BinaryHeap<T>::Remove() {
 	HeapifyDown(0);
 }
 
-template <typename T>
-void BinaryHeap<T>::HeapSort(std::vector<T>& vec, bool is_min_heap) {
+template <typename K, typename T>
+void BinaryHeap<K, T>::HeapSort(std::vector<std::pair<K, T>>& vec, bool is_min_heap) {
 	//The process of sorting results in a reversed array, so is_min_heap is negated.
 	for (int i = vec.size() / 2; i >= 0; i--) {
 		HeapifyDown(vec, vec.size(), i, !is_min_heap);
@@ -93,30 +93,30 @@ void BinaryHeap<T>::HeapSort(std::vector<T>& vec, bool is_min_heap) {
 	}
 }
 
-template <typename T>
-unsigned int BinaryHeap<T>::Size() const {
+template <typename K, typename T>
+unsigned int BinaryHeap<K, T>::Size() const {
 	return size;
 }
 
-template <typename T>
-bool BinaryHeap<T>::isMinHeap() const {
+template <typename K, typename T>
+bool BinaryHeap<K, T>::isMinHeap() const {
 	return is_min_heap;
 }
 
-template <typename T>
-void BinaryHeap<T>::Clear() {
+template <typename K, typename T>
+void BinaryHeap<K, T>::Clear() {
 	heap.clear();
 	size = 0;
 }
 
-template <typename T>
-bool BinaryHeap<T>::Compare(T& item1, T& item2) const {
+template <typename K, typename T>
+bool BinaryHeap<K, T>::Compare(K& item1, K& item2) const {
 	if (is_min_heap) return item1 < item2;
 	else return item1 > item2;
 }
 
-template <typename T>
-void BinaryHeap<T>::HeapifyDown(unsigned int index) {
+template <typename K, typename T>
+void BinaryHeap<K, T>::HeapifyDown(unsigned int index) {
 	int parent = index;
 	int favored = parent;
 
@@ -125,22 +125,22 @@ void BinaryHeap<T>::HeapifyDown(unsigned int index) {
 		int left_child = 2 * parent + 1;
 		int right_child = left_child + 1;
 
-		if (left_child < size && Compare(heap[left_child], heap[favored])) favored = left_child;
-		if (right_child < size && Compare(heap[right_child], heap[favored])) favored = right_child;
+		if (left_child < size && Compare(heap[left_child].first, heap[favored].first)) favored = left_child;
+		if (right_child < size && Compare(heap[right_child].first, heap[favored].first)) favored = right_child;
 
 		std::swap(heap[parent], heap[favored]);
 
 	} while (favored != parent);
 }
 
-template <typename T>
-bool BinaryHeap<T>::Compare(T& item1, T& item2, bool is_min_heap) {
+template <typename K, typename T>
+bool BinaryHeap<K, T>::Compare(K& item1, K& item2, bool is_min_heap) {
 	if (is_min_heap) return item1 < item2;
 	else return item1 > item2;
 }
 
-template <typename T>
-void BinaryHeap<T>::HeapifyDown(std::vector<T>& vec, unsigned int size, unsigned int index, bool is_min_heap) {
+template <typename K, typename T>
+void BinaryHeap<K, T>::HeapifyDown(std::vector<std::pair<K, T>>& vec, unsigned int size, unsigned int index, bool is_min_heap) {
 	int parent = index;
 	int favored = parent;
 
@@ -149,8 +149,8 @@ void BinaryHeap<T>::HeapifyDown(std::vector<T>& vec, unsigned int size, unsigned
 		int left_child = 2 * parent + 1;
 		int right_child = left_child + 1;
 
-		if (left_child < size && Compare(vec[left_child], vec[favored], is_min_heap)) favored = left_child;
-		if (right_child < size && Compare(vec[right_child], vec[favored], is_min_heap)) favored = right_child;
+		if (left_child < size && Compare(vec[left_child].first, vec[favored].first, is_min_heap)) favored = left_child;
+		if (right_child < size && Compare(vec[right_child].first, vec[favored].first, is_min_heap)) favored = right_child;
 
 		std::swap(vec[parent], vec[favored]);
 
